1f77efb55021239ad63e32ef7e193516fd7b0bd8
Jcg408 <jcg321@comcas.net>
2019-05-08 12:50:28 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,33 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+  // "ArrowUp",
+  // "ArrowUp",
+  // "ArrowDown",
+  // "ArrowDown",
+  // "ArrowLeft",
+  // "ArrowRight",
+  // "ArrowLeft",
+  // "ArrowRight",
+  // "b",
+  // "a"
 function init() {
-  // Write your JavaScript code inside the init() function
+ 
+    // Write your JavaScript code inside the init() function
+    let index = 0;
+  
+    document.body.addEventListener("keydown", function(e) {
+      const key = parseInt(e.detail || e.which);
+  
+      if (key === code[index]) {
+        index++;
+        if (index === code.length) {
+          alert("Congratulations, you have reached the secret path!");
+  
+          index = 0;
+        }
+      } else {
+        index = 0;
+      }
+    })
+  }
 
-}
+init();
